59710c160bab3008ec47b46b02c7bfa32e42058d
Goondrious <srs_34@hotmail.com>
2021-12-08 15:17:44 -0500

    Utils - useForm hook

--- hooks/utils.ts
+++ hooks/utils.ts
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState, useReducer, useEffect, useCallback } from "react";
 
 export const useList = <T>(init: T[] | null) => {
   const [list, setList] = useState(init || []);
@@ -108,4 +108,114 @@ export const useListViaAPI = <T>(
     removeViaAPI,
     editViaAPI,
   };
+};
+
+type Validators = { email: Function; name: Function };
+
+const VALIDATORS: Validators = {
+  email: (v: string) => !/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v),
+  name: (v: string) => v.length >= 3,
+  // password: e.g. strength calculation, check to see if it matches old pw
+};
+
+type Field<T> = {
+  key: string;
+  defaultValue: T;
+  validation: keyof Validators | ((o: T) => boolean);
+};
+
+type FormReducerState = {
+  [key: string]: {
+    value: any;
+    invalid?: boolean;
+  };
+};
+
+type FormReducerType = "update" | "fields" | keyof FormReducerState;
+
+const fieldsToFormObject = (
+  state: FormReducerState,
+  fields: Field<any>[]
+): FormReducerState =>
+  fields.reduce(
+    (agg, field) => ({
+      ...agg,
+      [field.key]: state[field.key] || { value: field.defaultValue },
+    }),
+    {}
+  );
+
+export const useForm = (fields: Field<any>[]) => {
+  const [form, dispatch] = useReducer(
+    (
+      state: FormReducerState,
+      { type, payload }: { type: FormReducerType; payload: any }
+    ) => {
+      if (type === "update") {
+        // update all keys from payload{}
+        // ====[TODO] depending on usage, what about "invalid" key?
+        return {
+          ...state,
+          ...Object.entries(state).reduce(
+            (agg, [key, currentState]) => ({
+              ...agg,
+              [key]: {
+                ...currentState,
+                ...payload[key],
+              },
+            }),
+            {}
+          ),
+        };
+      }
+
+      if (type === "fields") {
+        // reset the fields, keeping old values if they exist
+        return fieldsToFormObject(state, fields);
+      }
+
+      // default case is type === field.key
+      const field = fields.find((o) => o.key === type);
+
+      if (!field) {
+        // ====[TODO] throw error?
+        return state;
+      }
+
+      // ====[NOTE] provide a function or a key in VALIDATORS
+      let validationFn;
+      if (typeof field.validation === "function") {
+        validationFn = field.validation;
+      } else if (VALIDATORS[field.validation]) {
+        validationFn = VALIDATORS[field.validation];
+      }
+
+      return {
+        ...state,
+        [type]: {
+          value: payload,
+          invalid: validationFn ? validationFn(payload) : false,
+        },
+      };
+    },
+    fieldsToFormObject({}, fields)
+  );
+
+  useEffect(() => {
+    dispatch({ type: "fields", payload: fields });
+  }, [fields]);
+
+  const getFormAsObject = useCallback(
+    () =>
+      fields.reduce(
+        (agg, { key }) => ({
+          ...agg,
+          [key]: form[key].value,
+        }),
+        {}
+      ),
+    [fields, form]
+  );
+
+  return { form, getFormAsObject, dispatch };
 };
